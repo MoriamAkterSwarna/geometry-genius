ae93ebaa1afe4182cd6bbf2d2df488c1050cbaa0
MoriamAkterSwarna <moriam15-12880@diu.edu.bd>
2023-02-18 12:45:36 +0600

    js added

--- js/calculate.js
+++ js/calculate.js
@@ -1,33 +1,129 @@
 
-const areaListArray = [];
+    let serial = 0;
 
-// function for displaying the list of the selected player
-function displayAreaList() {
+    // triangle calculattion
+document.getElementById('triangle_calculate').addEventListener('click',function(e){
 
-    const areaList = document.getElementById('area-list');
-    areaList.textContent = "";
 
-    for (let i = 0; i < areaListArray.length; i++) {
+    serial += 1;
 
-            // create a table and append the child for displaying the player lists
-            const tr = document.createElement("tr");
-            tr.innerHTML = `
-                <th>${i + 1}</th>
-                <td>${areaListArray[i].areaName}</td>
-        
-            `;
-           areaList.appendChild(tr);
-        }
+    const area = getAllData(e);
+    const triangleArea = 0.5 * parseFloat(area.shapeValueOne) * parseFloat(area.shapeValueTwo);
+    const triangleAreaDecimal = triangleArea.toFixed(2);
+    // console.log(triangleArea);
+
+    displayData(area.shapeName, area.shapeValueOne,area.shapeValueTwo, triangleAreaDecimal);
+
+    
+})
+// rectangle calculation
+document.getElementById('rectangle-calculate').addEventListener('click',function(e){
+
+    serial += 1;
+
+    const area = getAllData(e);
+    const rectangleArea = parseFloat(area.shapeValueOne) * parseFloat(area.shapeValueTwo);
+    const rectangleAreaDecimal = rectangleArea.toFixed(2);
+
+    displayData(area.shapeName, area.shapeValueOne, area.shapeValueTwo,rectangleAreaDecimal);
+
+})
+// parallelogram calculation
+document.getElementById('parallelogram-calculate').addEventListener('click',function(e){
+
+    serial += 1;
+
+
+    const area = getAllData(e);
+    const parallelogramArea = parseFloat(area.shapeValueOne) * parseFloat(area.shapeValueTwo);
+    const parallelogramAreaDecimal = parallelogramArea.toFixed(2);
+
+    displayData(area.shapeName,area.shapeValueOne,area.shapeValueTwo,parallelogramAreaDecimal);
+
+})
+// Rhombus calculation
+document.getElementById('rhombus-calculate').addEventListener('click',function(e){
+
+    serial += 1;
+
+    const area = getAllData(e);
+    const rhombusArea = 0.5 * parseFloat(area.shapeValueOne) * parseFloat(area.shapeValueTwo);
+    const rhombusAreaDecimal = rhombusArea.toFixed(2);
+
+    displayData(area.shapeName,area.shapeValueOne,area.shapeValueTwo,rhombusAreaDecimal);
+
+})
+// Pentagon calculation
+document.getElementById('pentagon-calculate').addEventListener('click',function(e){
+
+    serial += 1;
+
+    const area = getAllData(e);
+    const pentagonArea = 0.5 * parseFloat(area.shapeValueOne) * parseFloat(area.shapeValueTwo);
+    const pentagonAreaDecimal = pentagonArea.toFixed(2);
+   
+    displayData(area.shapeName,area.shapeValueOne,area.shapeValueTwo,pentagonAreaDecimal);
+
+})
+// Ellipse calculation
+document.getElementById('ellipse-calculate').addEventListener('click',function(e){
+
+    serial += 1;
+
+    const area = getAllData(e);
+    const ellipseArea = 3.14* parseFloat(area.shapeValueOne) * parseFloat(area.shapeValueTwo);
+    const ellipseAreaDecimal = ellipseArea.toFixed(2);
+
+    displayData(area.shapeName,area.shapeValueOne,area.shapeValueTwo,ellipseAreaDecimal);
+
+})
+
+
+// common function
+
+function displayData(shapeName, shapeValueOne, shapeValueTwo, shapeArea){
+    // show the data
+    const container = document.getElementById("area-list");
+
+
+    const tr = document.createElement("tr");
+  tr.innerHTML = `
+        <td>${serial}</td>
+        <td>${shapeName}</td>
+        <td>${shapeValueOne}</td>
+        <td>${shapeValueTwo}</td>
+        <td>${shapeArea}</td>
+        <td>
+            <button class="btn btn-primary">Convert to  m<sup>2</sup></button>
+        </td>
+    
+  `;
+  container.appendChild(tr);
 }
-// function for select player 
-function calculate(element) {
-    const areaName = element.parentNode.children[0].innerText;
 
-    const area = {
-        areaName: areaName
-    }
-    areaListArray.push(area)
+// common function to get data using event object
 
-    displayAreaList();
+function getAllData(e) {
+    const shapeName = e.target.parentNode.children[0].innerText;
+    const shapeValueOne = e.target.parentNode.children[2].children[0].value;
+    const shapeValueTwo = e.target.parentNode.children[2].children[2].value;
 
-}
+  
+    const shape = {
+      shapeName: shapeName,
+      shapeValueOne: shapeValueOne,
+      shapeValueTwo: shapeValueTwo,
+    };
+
+    // input validation 
+    if (
+        shapeValueOne == "" ||
+        shapeValueTwo == "" ||
+        shapeValueOne <= 0 ||
+        shapeValueTwo <= 0
+      ) {
+        return alert("please enter any valid number");
+      }
+  
+    return shape;
+  }
